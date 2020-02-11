960a3e7ed37d5fafbca22ac57721db4261390722
Greg <51517739+VoytekP@users.noreply.github.com>
2020-02-11 21:27:32 +0100

    Add files via upload

---
+++ 20200211GalToLitMain.java
@@ -0,0 +1,43 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        int a, b, c;
+////
+////        a = 2;
+////        b = 3;
+////
+////        if (a < b) System.out.println("a is less then b");
+////
+////        if (a == b) System.out.println("you won't see this");
+////
+////        System.out.println();
+////
+////        c = a - b;
+////        System.out.println("c contains -1");
+////        if (c >= 0) System.out.println("c is non-negative");
+////        if (c < 0 ) System.out.println("c is negative");
+////
+////        System.out.println();
+////
+////        c = b - a;
+////        System.out.println("c contains 1");
+////
+////        if (c >= 0) System.out.println("c is non-negative");
+
+        double i, j, d;
+
+        i = 0;
+        j = 10;
+
+        if (i != 0) {
+            System.out.println(" i does not equal zero");
+            d = j / i;
+            System.out.println("j / i is " + d);
+        }
+
+
+    }
+}
