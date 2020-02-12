c4eb91b14c0e9bafdf316f814e7785f28b8224be
Greg <51517739+VoytekP@users.noreply.github.com>
2020-02-12 23:41:02 +0100

    Add files via upload

---
+++ 20200212Ch1SelfTest.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double weightOnEarth = 80;
+        double weightOnMoon = weightOnEarth * 0.17;
+
+        System.out.println("My weight on earth is " + weightOnEarth + " kg, and weight on the Moon would be  " +
+                 weightOnMoon + " kg"  );
+	// write your code here
+    }
+}
