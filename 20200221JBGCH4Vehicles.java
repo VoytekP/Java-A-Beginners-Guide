ba3d0e681645fd84857b05f3ea96e85943dee31e
Greg <51517739+VoytekP@users.noreply.github.com>
2020-02-21 22:48:58 +0100

    Add files via upload

---
+++ 20200221JBGCH4Vehicles.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Vehicle {
+
+        int passengers; // number of passengers
+        int fuelcap;    // fuel capacity in gallons
+        int mpg;        // fuel consumpitn in miles per gallon
+
+        // Display the range.
+        void range() {
+            System.out.println("Range is " + fuelcap * mpg);
+        }
+}
