b2ac179ecd515f3645edf09e0a2b39d535c926aa
Greg <51517739+VoytekP@users.noreply.github.com>
2020-03-05 06:30:04 +0100

    Add files via upload

---
+++ 20200305JBGCh6FailSoftArray.java
@@ -0,0 +1,42 @@
+package academy.learnprogramming;
+/*
+This class implements a "fails-soft" array which prevents runtime errors.
+ */
+
+public class FailSoftArray {
+    private int a[]; // reference to array
+    private int errval; // value to return if get() fails
+    public int length; // length is public
+
+    /*
+    Construct array given its size and the value to return if get() fails.
+     */
+    public FailSoftArray(int size, int errv) {
+        a = new int[size];
+        errval = errv;
+        length = size;
+    }
+    // Return value at given index.
+    public int get(int index) {
+        if (indexOK(index)) return a[index];
+        return errval;
+    }
+
+    // Put value at an index. Return false on failure.
+    public boolean put(int index, int val) {
+        if (indexOK(index)) {
+            a[index] = val;
+            return true;
+        }
+        return false;
+    }
+
+    // Return true if index is within bounds.
+    private boolean indexOK(int index) {
+        if (index >= 0 & index < length)
+            return true;
+        return false;
+    }
+}
+
+
