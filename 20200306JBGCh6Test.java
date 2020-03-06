e7c27ff9a26c8375b21f2578394641eb28ab17ae
Greg <51517739+VoytekP@users.noreply.github.com>
2020-03-06 22:51:08 +0100

    Add files via upload

---
+++ 20200306JBGCh6Test.java
@@ -0,0 +1,29 @@
+package academy.learnprogramming;
+
+// Primitive types are passed by value.
+
+public class Test {
+    /*
+    This method causes no change to the arguments used in the call.
+     */
+    void noChange(int i, int j) {
+        i = i + j;
+        j = -j;
+    }
+}
+
+class CallByValue {
+    public static void main(String[] args) {
+        Test ob = new Test();
+
+        int a = 15, b = 20;
+
+        System.out.println("a and b before call: " +
+                a + " " + b);
+
+        ob.noChange(a, b);
+
+        System.out.println("a and b after call: " +
+                a + " " + b);
+    }
+}
