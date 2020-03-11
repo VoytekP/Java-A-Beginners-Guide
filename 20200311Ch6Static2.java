e95e9a5aed0033e6e870a26a53518a9634502582
Greg <51517739+VoytekP@users.noreply.github.com>
2020-03-11 22:16:02 +0100

    Add files via upload

---
+++ 20200311Ch6Static2.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming;
+
+public class StaticMeth {
+    static int val = 1024;
+
+    static int valDiv2() {
+        return val/2;
+    }
+}
---
+++ 20200311Ch6Static2Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class SDemo2 {
+    public static void main(String[] args) {
+
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " +
+                StaticMeth.valDiv2());
+
+        StaticMeth.val = 4;
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " +
+                StaticMeth.valDiv2());
+    }
+}
