e2fef6def69b49ad897861c53f681dc790deab1f
Greg <51517739+VoytekP@users.noreply.github.com>
2020-03-17 20:57:32 +0100

    Add files via upload

---
+++ 20200317Ch7Shapes7DShapes.java
@@ -0,0 +1,40 @@
+package academy.learnprogramming;
+
+public class TwoDShape {
+    private double width;
+    private double height;
+
+    // A default constructor.
+    TwoDShape() {
+        width = height = 0.0;
+    }
+
+    // Parameterized constructor.
+    TwoDShape(double w, double h) {
+        width = w;
+        height = h;
+    }
+
+    // Construct an object with equal width and height.
+    TwoDShape(double x) {
+        width = height = x;
+    }
+
+    // Construct an object with equal width and height.
+    TwoDShape(TwoDShape ob) {
+        width = ob.width;
+        height = ob.height;
+    }
+
+    // Accessor methods for width and height.
+
+    double getWidth() { return width; }
+    double getHeight() { return height; }
+    void setWidth(double w) { width = w; }
+    void setHeight(double h) { height = h; }
+
+    void showDim() {
+        System.out.println("Width and height are " +
+                            width + " and " + height);
+    }
+}
