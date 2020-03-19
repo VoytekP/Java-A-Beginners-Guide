802c9a3bed1e87a626d0aad8f22bc188edf0f628
Greg <51517739+VoytekP@users.noreply.github.com>
2020-03-19 21:41:13 +0100

    Add files via upload

---
+++ 20200319JBGCh8IQDemoICharQInterface.java
@@ -0,0 +1,10 @@
+package java.beginners.guide.book;
+// A character queue interface.
+
+public interface ICharQ {
+    // Put a character into toe queue.
+    void put(char ch);
+
+    // Get a character from the queue.
+    char get();
+}
