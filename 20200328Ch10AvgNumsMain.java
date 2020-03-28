67cd056ae2bf3366c3cdce1d09cdbd6f5f723ec9
Greg <51517739+VoytekP@users.noreply.github.com>
2020-03-28 11:27:42 +0100

    Add files via upload

---
+++ 20200328Ch10AvgNumsMain.java
@@ -0,0 +1,41 @@
+package guide.chapter.ten;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// This program averages a list of numbers entered by the user
+public class AvgNums {
+    public static void main(String[] args) throws IOException {
+        // crate a BufferedReader using System.in
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        int n;
+        double sum = 0.0;
+        double avg, t;
+
+        System.out.print("How many numbers will you enter: ");
+        str = br.readLine();
+        try {
+            n = Integer.parseInt(str);
+        } catch (NumberFormatException exc) {
+            System.out.println("Invalid format");
+            n = 0;
+        }
+
+        System.out.println("Enter " + n + " values.");
+        for (int i = 0; i < n; i++) {
+            System.out.print(": ");
+            str = br.readLine();
+            try {
+                t = Double.parseDouble(str); // convert string to double.
+            } catch (NumberFormatException exc) {
+                System.out.println("Invalid format" );
+                t = 0.0;
+            }
+            sum += t;
+        }
+        avg = sum / n;
+        System.out.println("Average is " + avg);
+    }
+}
---
+++ 20200328Ch10FileWriterMain.java
@@ -0,0 +1,31 @@
+package guide.chapter.ten;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KtoD {
+    public static void main(String[] args) {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter text ('stop to quit).");
+
+        try (FileWriter fw = new FileWriter("test.text"))
+        {
+            do {
+                System.out.print(": ");
+                str = br.readLine();
+                
+                if (str.compareTo("stop") == 0)
+                    break;
+
+                str = str + "\r\n"; // add newline
+                fw.write(str);
+            } while (str.compareTo("stop") != 0);
+        } catch (IOException exc) {
+            System.out.println("I/O Error: " + exc);
+        }
+    }
+}
