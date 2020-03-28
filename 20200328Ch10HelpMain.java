f4d3b7fa0e4a205d7d474e4eb68380a9244d2bcb
Greg <51517739+VoytekP@users.noreply.github.com>
2020-03-28 12:04:53 +0100

    Add files via upload

---
+++ 20200328Ch10HelpMain.java
@@ -0,0 +1,16 @@
+package guide.chapter.ten;
+// Demonstrate the file-based Help system.
+public class FileHelp {
+    public static void main(String[] args) {
+        Help hlpobj = new Help("helpfile.txt");
+        String topic;
+
+        System.out.println("Try the help system. " +
+                            "Enter 'stop' to end.");
+        do {
+            topic = hlpobj.getSelection();
+            if (!hlpobj.helpOn(topic))
+                System.out.println("Topic not found.\n");
+        } while (topic.compareTo("stop") != 0);
+    }
+}
